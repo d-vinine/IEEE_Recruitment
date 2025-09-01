72f72f337aee5148fcd71c762da7d2eda096f5e8
d-vinine <dhanvinmalhotra10@gmail.com>
2025-09-01 13:22:26 +0000

    Edited question_5.py

--- IEEE_Recruitment_Task_1/question_5.py
+++ IEEE_Recruitment_Task_1/question_5.py
@@ -1,21 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.random.normal(0, 1, 1000) # generates random points sampled from a normal distribution
+x = np.random.normal(0, 1, 1000) # generates random points sampled from a normal distribution
+y = np.random.normal(0, 1, 1000) 
 
 plt.title("Scatter Plot of a 1000 Points Sampled From a Normal Distribution")
 
 plt.scatter(
-    x = [i for i in range(1000)],
-    y = data,
+    x = x,
+    y = y,
     c = "green",
     s = 16,
     linewidth = 0.1,
     marker = "X"
 ) # create the scatter plot
 
-plt.xlabel("Index")
-plt.ylabel("Sampled Value")
+plt.xlabel("X")
+plt.ylabel("Y")
 
 plt.grid(linestyle='-', linewidth=.5)
 
